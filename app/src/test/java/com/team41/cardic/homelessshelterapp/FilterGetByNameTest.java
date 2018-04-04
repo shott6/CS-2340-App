dc5a54df8ac84658551cde05823e30be39f02b20
github username <shott@hewittschool.org>
2018-04-04 13:55:30 -0400

    Added Javadocs for FilterGetByNameTest

--- app/src/test/java/com/team41/cardic/homelessshelterapp/FilterGetByNameTest.java
+++ app/src/test/java/com/team41/cardic/homelessshelterapp/FilterGetByNameTest.java
@@ -28,7 +28,9 @@ public class FilterGetByNameTest {
 
     private final Model model = Model.getInstance();
 
-
+    /**
+     * Method that sets up the shelters array list in model to contain test shelters with different names
+     */
     @Before
     public void setup() {
         model.setShelters(new ArrayList<Shelter>());
@@ -37,6 +39,10 @@ public class FilterGetByNameTest {
         model.addShelter(new Shelter(2, "no", "", "", LONG, LAT, "", "", ""));
     }
 
+    /**
+     * Method that tests to make sure filter's getByName method works when there is only one shelter whose name matches
+     * It also makes sure that the size of the associated array list is the correct size
+     */
     @Test
     public void testNameEquality1() {
         correctShelts[0] = name;
@@ -48,6 +54,10 @@ public class FilterGetByNameTest {
         }
     }
 
+    /**
+     * Method that tests to make sure filter's getByName method works when there is more than one shelter whose name matches
+     * It also makes sure that the size of the associated array list is the correct size
+     */
     @Test
     public void testNameEquality2() {
         model.addShelter(new Shelter(3, name, "", "", LONG, LAT, "", "", ""));
@@ -61,6 +71,9 @@ public class FilterGetByNameTest {
         }
     }
 
+    /**
+     * Method that makes sure filter's getByName method works when there are no shelters in the model list
+     */
     @Test
     public void testEmpty() {
         model.setShelters(new ArrayList<Shelter>());
