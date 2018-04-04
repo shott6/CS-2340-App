1761f775b84ef44985fca6e8976c10e7f868ae2b
Nyima Piaro <benkucera@gmail.com>
2018-04-03 21:01:28 -0400

    fixed dangling javadoc comment

--- app/src/main/java/com/team41/cardic/homelessshelterapp/model/Shelter.java
+++ app/src/main/java/com/team41/cardic/homelessshelterapp/model/Shelter.java
@@ -39,8 +39,9 @@ public class Shelter {
         phoneNumber = _phoneNumber;
     }
 
-    /**
-     * Getters and setters
+    /*
+    Getters and setters
+    -------------------
      */
 
     /**
