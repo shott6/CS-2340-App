702c9a55945d3cf224241e1373d0496dfd8f7371
DavidNeil <dndaniell16@gmail.com>
2018-04-09 18:56:57 -0400

    fixed warnings for getMaleOnly()

--- app/src/test/java/com/team41/cardic/homelessshelterapp/FilterGetMaleOnlyTest.java
+++ app/src/test/java/com/team41/cardic/homelessshelterapp/FilterGetMaleOnlyTest.java
@@ -14,13 +14,9 @@ import java.util.ArrayList;
 public class FilterGetMaleOnlyTest {
     private final Filter filter = new Filter();
 
-    private final String name = "Shelter1";
-    private final double LONG = 3.14;
-    private final double LAT = 1.59;
     private final String nameGender = "Men";
     private final String nameGender2 = "anyGender";
     private final String[] correctShelters = new String[2];
-    private int correctSheltersSize;
 
     private final Model model = Model.getInstance();
 
@@ -30,6 +26,9 @@ public class FilterGetMaleOnlyTest {
     @Before
     public void setup() {
         model.setShelters(new ArrayList<Shelter>());
+        String name = "Shelter1";
+        double LONG = 3.14;
+        double LAT = 1.59;
         model.addShelter(new Shelter(0, name, "", nameGender, LONG, LAT, "", "", ""));
         model.addShelter(new Shelter(1, "a name", "Women", "", LONG, LAT, "", "", ""));
         model.addShelter(new Shelter(2, "name2", "", nameGender2, LONG, LAT, "", "", ""));
@@ -44,7 +43,7 @@ public class FilterGetMaleOnlyTest {
     public void testGenderEquality() {
         correctShelters[0] = nameGender;
         correctShelters[1] = nameGender2;
-        correctSheltersSize = 2;
+        int correctSheltersSize = 2;
         filter.getMaleOnly();
         for (int i = 0; i < filter.getFilteredShelters().size(); i++) {
             Assert.assertEquals(correctShelters[i], filter.getFilteredShelters().get(i).getGender());
